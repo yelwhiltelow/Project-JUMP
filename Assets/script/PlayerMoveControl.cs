a21984aca26cfd0ea1813ce8851edd37e3a2f14b
yayantt <yangsy0801@gmail.com>
2025-11-28 15:12:34 +0900

    이동

--- Assets/script/PlayerMoveControl.cs
+++ Assets/script/PlayerMoveControl.cs
@@ -8,11 +8,7 @@ public class PlayerMoveControl : MonoBehaviour
     public float jumpForce = 7.0f;   // 점프 힘
 
     float horizontal;                // 좌우 입력값
-    Rigidbody2D rb;                  // Rigidbody2D 참조
-    bool isGrounded;                 // 땅에 닿아있는지 체크
-
-    public Transform groundCheck;    // 땅 체크용 Transform (발밑 위치)
-    public LayerMask groundLayer;    // 땅 레이어 지정
+    public Rigidbody2D rb;                  // Rigidbody2D 참조
 
     private void Awake()
     {
@@ -22,7 +18,6 @@ public class PlayerMoveControl : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
-        CheckGround();
     }
 
     private void Update()
@@ -38,16 +33,10 @@ public class PlayerMoveControl : MonoBehaviour
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("Jump"))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-    }
-
-    void CheckGround()
-    {
-        // 발밑에 작은 원을 그려서 땅과 닿아있는지 확인
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
     }
 }
 
